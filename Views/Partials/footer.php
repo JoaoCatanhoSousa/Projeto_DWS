767c8bf990459d31ea7e85662ebcdf0ec4822afe
JoaoCatanho <2117722@student.uma.pt>
2024-11-07 22:45:40 +0100

    adicionei o footer

---
+++ Views/Partials/footer.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ *Importante information 
+ !Deprecated method, do not use
+ TODO: refactor this code
+ ?should this method be used?
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/Public/style.css"> <!-- Link para o arquivo CSS -->
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Footer-->
+    <footer class="bg-dark py-4 mt-auto">
+        <div class="container px-5">
+            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                <div class="col-auto">
+                    <small class="m-0 text-white">Copyright &copy; Your Website 2024</small>
+                </div>
+                <div class="col-auto">
+                    <a class="link-light small" href="#!">Contact Us</a>
+                    <span class="text-white mx-1">&middot;</span>
+                    <a class="link-light small" href="#!">About Us</a>
+                    <span class="text-white mx-1">&middot;</span>
+                    <a class="link-light small" href="#!">Terms</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+<!-- Bootstrap JS and dependencies -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+
+</body>
+</html>
