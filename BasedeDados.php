c080422df0cc1fedfe6e76b3f71cb1ee2301ce8a
JoaoCatanho <2117722@student.uma.pt>
2024-11-07 01:02:47 +0100

    mudei o nome do projeto

--- BasedeDados.php
+++ BasedeDados.php
@@ -3,7 +3,7 @@
     $servername = "localhost";  // Servidor do MySQL (localhost quando se usa o XAMPP)
     $username = "root";         // Nome de usuário padrão do MySQL no XAMPP
     $password = "";             // Senha padrão do MySQL no XAMPP é vazia
-    $dbname = "project_dws"; // Nome da base de dados que você criou
+    $dbname = "Projeto_DWS";    // Nome da base de dados que você criou
 
     // Criar a conexão
     $conn = new mysqli($servername, $username, $password, $dbname);
