c1ccbcb6526b7bdb91edb46a1a9386d5b4bb3007
JoaoCatanho <2117722@student.uma.pt>
2024-12-23 17:09:58 +0000

    modifiquei o caminho do botão para a home page

--- Views/Partials/header.php
+++ Views/Partials/header.php
@@ -27,7 +27,7 @@
             
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="homePage.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="aboutUs.php">About Us</a></li>
                     <li class="nav-item"><a class="nav-link" href="contactUs.php">Contact Us</a></li>
                     <li class="nav-item"><a class="nav-link" href="terms.php">Terms</a></li>
